342f5610d03f0993c07861b2b27c5449e561e229
adityagrg <31734655+adityagrg@users.noreply.github.com>
2017-09-08 17:14:38 +0530

    Update __init__.py

--- pyredis/__init__.py
+++ pyredis/__init__.py
@@ -73,7 +73,7 @@ def get_by_url(url):
             return PubSubClient(host=host, port=port, **kwargs)
         else:
             raise PyRedisURLError("invalid schema: {0}")
-    except TypeError as err:
+    except TypeError, err:
         raise PyRedisURLError("unexpected or missing options specified: {0}".format(err))
 
 
