f85c66b6795f5e65427d58bfd891840b1f2278cb
adityagrg <31734655+adityagrg@users.noreply.github.com>
2017-09-10 00:32:11 +0530

    Update helper.py

--- pyredis/helper.py
+++ pyredis/helper.py
@@ -1,3 +1,5 @@
+from __future__ import with_statement
+from __future__ import absolute_import
 import crc16
 import random
 from collections import deque
@@ -7,14 +9,15 @@ from uuid import uuid4
 from pyredis.connection import Connection
 from pyredis.exceptions import PyRedisError
 from pyredis.protocol import to_bytes
+from itertools import izip
 
 
 def dict_from_list(source):
-    return dict(zip(*[iter(source)]*2))
+    return dict(izip(*[iter(source)]*2))
 
 
 def tag_from_key(key):
-    """ return tag from key
+    u""" return tag from key
 
     Tries to convert key to bytes, and return the string
     enclosed by '{' and '}', if any.
@@ -25,8 +28,8 @@ def tag_from_key(key):
     :return: bytes
     """
     key = to_bytes(key)
-    lcb = key.find(b'{')
-    rcb = key.find(b'}')
+    lcb = key.find('{')
+    rcb = key.find('}')
     if not (lcb >= 0 and rcb >= 0):
         return key
     else:
@@ -50,38 +53,38 @@ class ClusterMap(object):
 
     @staticmethod
     def _make_str(endpoint):
-        return str(endpoint[0])+'_'+str(endpoint[1])
+        return unicode(endpoint[0])+u'_'+unicode(endpoint[1])
 
     def _fetch_map(self):
         for seed in self._seeds:
-            conn = Connection(host=seed[0], port=seed[1], encoding='utf-8')
+            conn = Connection(host=seed[0], port=seed[1], encoding=u'utf-8')
             try:
-                conn.write('CLUSTER', 'SLOTS')
+                conn.write(u'CLUSTER', u'SLOTS')
                 return conn.read()
             except PyRedisError:
                 pass
             finally:
                 conn.close()
-        raise PyRedisError('Could not get cluster info from any seed node: {0}'.format(self._seeds))
+        raise PyRedisError(u'Could not get cluster info from any seed node: {0}'.format(self._seeds))
 
     def _update_slot(self, slot, master, slaves):
         self._map[slot] = {
-            'master': self._make_str(master),
-            'slave': self._make_str(random.choice(slaves))
+            u'master': self._make_str(master),
+            u'slave': self._make_str(random.choice(slaves))
         }
 
     def get_slot(self, shard_key, slave=None):
         if not slave:
-            return self._map[slot_from_key(shard_key)]['master']
+            return self._map[slot_from_key(shard_key)][u'master']
         else:
-            return self._map[slot_from_key(shard_key)]['slave']
+            return self._map[slot_from_key(shard_key)][u'slave']
 
     def hosts(self, slave=None):
         result = set()
         if not slave:
-            selector = 'master'
+            selector = u'master'
         else:
-            selector = 'slave'
+            selector = u'slave'
         for host in self._map.values():
             result.add(host[selector])
         return result
@@ -91,7 +94,7 @@ class ClusterMap(object):
             if id != self.id:
                 return self.id
             for entry in self._fetch_map():
-                for slot in range(entry[0], entry[1]+1):
+                for slot in xrange(entry[0], entry[1]+1):
                     self._update_slot(slot, entry[2], entry[3:])
             self._id = uuid4()
             return self.id
