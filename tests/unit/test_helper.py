8d2b17e935f5ed0f6378a1d662debb1f2e11cc98
adityagrg <31734655+adityagrg@users.noreply.github.com>
2017-09-10 10:57:30 +0530

    Update test_helper.py

--- tests/unit/test_helper.py
+++ tests/unit/test_helper.py
@@ -1,4 +1,5 @@
-__author__ = 'schlitzer'
+from __future__ import absolute_import
+__author__ = u'schlitzer'
 
 from collections import deque
 from unittest import TestCase
@@ -12,44 +13,44 @@ from pyredis.helper import dict_from_list, tag_from_key, slot_from_key, ClusterM
 
 class TestHelperUnit(TestCase):
     def test_dict_from_list(self):
-        source = ['test1', 1234, 'test2', 5678]
+        source = [u'test1', 1234, u'test2', 5678]
         expected = {
-            'test1': 1234,
-            'test2': 5678
+            u'test1': 1234,
+            u'test2': 5678
         }
         result = dict_from_list(source)
         self.assertEqual(result, expected)
 
     def test_tag_from_key_no_tag(self):
-        key = 'testkey'
+        key = u'testkey'
         result = tag_from_key(key)
         self.assertEqual(key.encode(), result)
         pass
 
     def test_tag_from_key_1lcb_1rcb(self):
-        key = 'blarg{testtag}sdfsdf'
-        tag = b'testtag'
+        key = u'blarg{testtag}sdfsdf'
+        tag = 'testtag'
         result = tag_from_key(key)
         self.assertEqual(tag, result)
 
     def test_tag_from_key_1lcb_0rcb(self):
-        key = '{asdas'
+        key = u'{asdas'
         result = tag_from_key(key)
         self.assertEqual(key.encode(), result)
 
     def test_tag_from_key_0lcb_1rcb(self):
-        key = 'sadasd}a'
+        key = u'sadasd}a'
         result = tag_from_key(key)
         self.assertEqual(key.encode(), result)
 
     def test_tag_from_key_2lcb_2rcb(self):
-        key = 'blarg{{testtag}sd}fsdf'
-        tag = b'{testtag'
+        key = u'blarg{{testtag}sd}fsdf'
+        tag = '{testtag'
         result = tag_from_key(key)
         self.assertEqual(tag, result)
 
     def test_slot_from_key(self):
-        key = 'blarg'.encode()
+        key = u'blarg'.encode()
         result = slot_from_key(key)
         self.assertEqual(5534, result)
 
@@ -58,48 +59,48 @@ class TestClusterMap(TestCase):
     def setUp(self):
         self.addCleanup(patch.stopall)
 
-        connection_patcher = patch('pyredis.helper.Connection', autospec=True)
+        connection_patcher = patch(u'pyredis.helper.Connection', autospec=True)
         self.connection_mock = connection_patcher.start()
 
-        self.seeds = [('host1', 12345), ('host2', 12345), ('host3', 12345)]
+        self.seeds = [(u'host1', 12345), (u'host2', 12345), (u'host3', 12345)]
         self.map = [
             [
                 0,
                 5460,
-                ['127.0.0.1', 7000],
-                ['127.0.0.1', 7003]
+                [u'127.0.0.1', 7000],
+                [u'127.0.0.1', 7003]
             ],
             [
                 5461,
                 10922,
-                ['127.0.0.1', 7001],
-                ['127.0.0.1', 7004]
+                [u'127.0.0.1', 7001],
+                [u'127.0.0.1', 7004]
             ],
             [
                 10923,
                 16383,
-                ['127.0.0.1', 7002],
-                ['127.0.0.1', 7005]
+                [u'127.0.0.1', 7002],
+                [u'127.0.0.1', 7005]
             ]
         ]
         self.minimap = [
             [
                 0,
                 2,
-                ['127.0.0.1', 7000],
-                ['127.0.0.1', 7003]
+                [u'127.0.0.1', 7000],
+                [u'127.0.0.1', 7003]
             ],
             [
                 3,
                 3,
-                ['127.0.0.1', 7001],
-                ['127.0.0.1', 7004]
+                [u'127.0.0.1', 7001],
+                [u'127.0.0.1', 7004]
             ],
             [
                 4,
                 5,
-                ['127.0.0.1', 7002],
-                ['127.0.0.1', 7005]
+                [u'127.0.0.1', 7002],
+                [u'127.0.0.1', 7005]
             ]
         ]
 
@@ -109,8 +110,8 @@ class TestClusterMap(TestCase):
         self.assertEqual(clustermap._seeds, deque(self.seeds))
 
     def test__make_str(self):
-        result = ClusterMap._make_str(['127.0.0.1', 7002])
-        self.assertEqual(result, '127.0.0.1_7002')
+        result = ClusterMap._make_str([u'127.0.0.1', 7002])
+        self.assertEqual(result, u'127.0.0.1_7002')
 
     def test__fetch_map_first_try_ok(self):
         conn1 = Mock()
@@ -123,7 +124,7 @@ class TestClusterMap(TestCase):
         result = clustermap._fetch_map()
 
         self.assertEqual(result, self.map)
-        conn1.write.assert_called_with('CLUSTER', 'SLOTS')
+        conn1.write.assert_called_with(u'CLUSTER', u'SLOTS')
         self.assertTrue(conn1.close.called)
 
     def test__fetch_map_second_try_ok(self):
@@ -139,9 +140,9 @@ class TestClusterMap(TestCase):
         result = clustermap._fetch_map()
 
         self.assertEqual(result, self.map)
-        conn1.write.assert_called_with('CLUSTER', 'SLOTS')
+        conn1.write.assert_called_with(u'CLUSTER', u'SLOTS')
         self.assertTrue(conn1.close.called)
-        conn2.write.assert_called_with('CLUSTER', 'SLOTS')
+        conn2.write.assert_called_with(u'CLUSTER', u'SLOTS')
         self.assertTrue(conn2.close.called)
 
     def test__fetch_map_exception(self):
@@ -160,16 +161,16 @@ class TestClusterMap(TestCase):
         self.assertRaises(PyRedisError, clustermap._fetch_map)
 
         self.connection_mock.assert_has_calls([
-            call(host='host1', port=12345, encoding='utf-8'),
-            call(host='host2', port=12345, encoding='utf-8'),
-            call(host='host3', port=12345, encoding='utf-8')
+            call(host=u'host1', port=12345, encoding=u'utf-8'),
+            call(host=u'host2', port=12345, encoding=u'utf-8'),
+            call(host=u'host3', port=12345, encoding=u'utf-8')
         ])
 
-        conn1.write.assert_called_with('CLUSTER', 'SLOTS')
+        conn1.write.assert_called_with(u'CLUSTER', u'SLOTS')
         self.assertTrue(conn1.close.called)
-        conn2.write.assert_called_with('CLUSTER', 'SLOTS')
+        conn2.write.assert_called_with(u'CLUSTER', u'SLOTS')
         self.assertTrue(conn2.close.called)
-        conn3.write.assert_called_with('CLUSTER', 'SLOTS')
+        conn3.write.assert_called_with(u'CLUSTER', u'SLOTS')
         self.assertTrue(conn3.close.called)
 
     def test_update(self):
@@ -182,12 +183,12 @@ class TestClusterMap(TestCase):
         id_new = clustermap.update(clustermap.id)
         clustermap._update_slot.assert_has_calls(
             [
-                call(0, ['127.0.0.1', 7000], [['127.0.0.1', 7003]]),
-                call(1, ['127.0.0.1', 7000], [['127.0.0.1', 7003]]),
-                call(2, ['127.0.0.1', 7000], [['127.0.0.1', 7003]]),
-                call(3, ['127.0.0.1', 7001], [['127.0.0.1', 7004]]),
-                call(4, ['127.0.0.1', 7002], [['127.0.0.1', 7005]]),
-                call(5, ['127.0.0.1', 7002], [['127.0.0.1', 7005]])
+                call(0, [u'127.0.0.1', 7000], [[u'127.0.0.1', 7003]]),
+                call(1, [u'127.0.0.1', 7000], [[u'127.0.0.1', 7003]]),
+                call(2, [u'127.0.0.1', 7000], [[u'127.0.0.1', 7003]]),
+                call(3, [u'127.0.0.1', 7001], [[u'127.0.0.1', 7004]]),
+                call(4, [u'127.0.0.1', 7002], [[u'127.0.0.1', 7005]]),
+                call(5, [u'127.0.0.1', 7002], [[u'127.0.0.1', 7005]])
             ]
         )
         self.assertNotEqual(clustermap.id, id)
@@ -195,10 +196,10 @@ class TestClusterMap(TestCase):
 
     def test_update_slot(self):
         clustermap = ClusterMap(self.seeds)
-        clustermap._update_slot(0, ['127.0.0.1', 7000], [['127.0.0.1', 7003]])
+        clustermap._update_slot(0, [u'127.0.0.1', 7000], [[u'127.0.0.1', 7003]])
         self.assertEqual(
             clustermap._map[0],
-            {'master': '127.0.0.1_7000', 'slave': '127.0.0.1_7003'}
+            {u'master': u'127.0.0.1_7000', u'slave': u'127.0.0.1_7003'}
         )
 
     def test_get_slot_master(self):
@@ -206,16 +207,16 @@ class TestClusterMap(TestCase):
         clustermap._fetch_map = Mock()
         clustermap._fetch_map.return_value = self.map
         clustermap.update(clustermap.id)
-        result = clustermap.get_slot('blarg')
-        self.assertEqual(result, '127.0.0.1_7001')
+        result = clustermap.get_slot(u'blarg')
+        self.assertEqual(result, u'127.0.0.1_7001')
 
     def test_get_slot_slaves(self):
         clustermap = ClusterMap(self.seeds)
         clustermap._fetch_map = Mock()
         clustermap._fetch_map.return_value = self.map
         clustermap.update(clustermap.id)
-        result = clustermap.get_slot('blarg')
-        self.assertEqual(result, '127.0.0.1_7001')
+        result = clustermap.get_slot(u'blarg')
+        self.assertEqual(result, u'127.0.0.1_7001')
 
     def test_hosts_master(self):
         clustermap = ClusterMap(self.seeds)
@@ -223,7 +224,7 @@ class TestClusterMap(TestCase):
         clustermap._fetch_map.return_value = self.minimap
         clustermap.update(clustermap.id)
         self.assertEqual(
-            ({'127.0.0.1_7000', '127.0.0.1_7001', '127.0.0.1_7002'}),
+            (set([u'127.0.0.1_7000', u'127.0.0.1_7001', u'127.0.0.1_7002'])),
             clustermap.hosts())
 
     def test_hosts_slaves(self):
@@ -232,5 +233,5 @@ class TestClusterMap(TestCase):
         clustermap._fetch_map.return_value = self.minimap
         clustermap.update(clustermap.id)
         self.assertEqual(
-            ({'127.0.0.1_7003', '127.0.0.1_7004', '127.0.0.1_7005'}),
+            (set([u'127.0.0.1_7003', u'127.0.0.1_7004', u'127.0.0.1_7005'])),
             clustermap.hosts(slave=True))
