7eb33ed31a6bbed2ca66b85355a01cf902bd2256
adityagrg <31734655+adityagrg@users.noreply.github.com>
2017-09-10 09:57:46 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,34 +1,35 @@
+from __future__ import absolute_import
 from setuptools import setup
 
 setup(
-    name='python_redis',
-    version='0.1.7',
-    description='Redis Client',
-    long_description="""
+    name=u'python_redis',
+    version=u'0.1.7',
+    description=u'Redis Client',
+    long_description=u"""
 Redis Client implementation for Python 3.
 
 Copyright (c) 2016, Stephan Schultchen.
 
 License: MIT (see LICENSE for details)
     """,
-    packages=['pyredis'],
-    url='https://github.com/schlitzered/pyredis',
-    license='MIT',
-    author='schlitzer',
-    author_email='stephan.schultchen@gmail.com',
-    test_suite='test',
-    platforms='posix',
+    packages=[u'pyredis'],
+    url=u'https://github.com/schlitzered/pyredis',
+    license=u'MIT',
+    author=u'schlitzer',
+    author_email=u'stephan.schultchen@gmail.com',
+    test_suite=u'test',
+    platforms=u'posix',
     classifiers=[
-            'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python :: 3'
+            u'License :: OSI Approved :: MIT License',
+            u'Programming Language :: Python :: 3'
     ],
     setup_requires=[
-        'crc16'
+        u'crc16'
     ],
     install_requires=[
-        'crc16'
+        u'crc16'
     ],
     keywords=[
-        'redis'
+        u'redis'
     ]
 )
